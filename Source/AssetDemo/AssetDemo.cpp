0b36738cd828eb80a816cd1211f8cc8077338369
1995zmj <784178252@qq.com>
2022-08-03 11:27:40 +0800

    ....

--- Source/AssetDemo/AssetDemo.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AssetDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AssetDemo, "AssetDemo" );
- 
